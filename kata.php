fbab7872f0f8443fdc1e9bbcb80da7a72a53ad1e
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-07-04 08:47:47 +0200

    Add the new kata

---
+++ kata.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Les phrases suivantes ont été crypté avec l'algorithme du kata d'hier.
+ * à vous de les décoder.
+ */
+
+ $troubledPhrases = [
+    '!1015117p101rM111td101P9755101',
+    'V105v101 !101 PHP',
+    '!111r101m Ip5117m',
+    't111117t 1015t t111117j111117r5 111bj101t.',
+    'D105ct105111nn97105r101',
+    'P97105n 97117 ch111c111!97t',
+    'Ch111c111!97t105n101',
+    'Ec111c111nc101pt105111n'
+ ];
